1ec52fe2c0d8c8f030355b8e2f176152369d0bc2
Daria Calitis <60710963+dbcalitis@users.noreply.github.com>
2022-03-29 10:31:29 -0400

    Update code.py

--- code.py
+++ code.py
@@ -8,8 +8,8 @@ from machine import Pin
 import utime
 
 # Sets the pins for trigger and echo
-trigger = Pin(14, Pin.OUT)
-echo = Pin(15, Pin.IN)
+trigger = Pin(3, Pin.OUT)
+echo = Pin(2 Pin.IN)
 
 # Main function
 def ultra():
