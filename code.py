9da16fc46003d30c60a7238133f8546a4e8842d9
Daria Calitis <60710963+dbcalitis@users.noreply.github.com>
2022-03-29 10:02:26 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,34 @@
+# Created by: Daria Bernice Calitsi
+# Created on: March 2022
+# It detects the distance through the Ultrasonic Distance Sensor.
+
+# Imports Pin and utime
+from machine import Pin
+import utime
+
+# Sets the pins for trigger and echo
+trigger = Pin(14, Pin.OUT)
+echo = Pin(15, Pin.IN)
+
+# Main function
+def ultra():
+   trigger.low()
+   utime.sleep_us(2)
+   trigger.high()
+   utime.sleep_us(5)
+   trigger.low()
+
+   while echo.value() == 0:
+       signaloff = utime.ticks_us()
+
+   while echo.value() == 1:
+       signalon = utime.ticks_us()
+
+   timepassed = signalon - signaloff
+   distance = (timepassed * 0.0343) / 2
+   print("Distance: ", distance, "cm")
+
+# Runs the code indefinitely
+while True:
+   ultra()
+   utime.sleep(1)
