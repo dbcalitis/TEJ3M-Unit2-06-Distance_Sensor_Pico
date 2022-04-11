8c47dec797c8999b7f07cd8b58816084b3960aa2
Daria Calitis <60710963+dbcalitis@users.noreply.github.com>
2022-04-11 11:07:23 -0400

    Update code.py

--- code.py
+++ code.py
@@ -33,3 +33,16 @@ def ultra():
 while True:
    ultra()
    utime.sleep(1)
+"""
+This one might be the right code...
+
+import time
+import board
+import adafruit_hcsr04
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP13, echo_pin=board.GP14)
+
+while True:
+    print((sonar.distance,))
+    time.sleep(0.1)
+    
+"""
